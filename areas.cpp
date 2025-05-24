d173ea27a83a9a7128a92fcfaf688f3e45a7a79c
vinicius <vinicyusalves05@gmail.com>
2025-05-24 13:58:30 -0300

    final

--- areas.cpp
+++ areas.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -13,7 +14,7 @@ class Ponto{
     Ponto(int x, int y) : _x(x), _y(y){};
 
     int getX() const {return _x; }
-    int getX() const {return _y; }
+    int getY() const {return _y; }
 
     void setX(int x){
         this->_x = x;
@@ -30,7 +31,8 @@ class FiguraGeometrica {
 
     public:
     FiguraGeometrica(int x, int y) : A(x,y){}
-    virtual void Desenha();
+    Ponto getPontoA() {return A; }
+    virtual void Desenha() = 0;
     virtual float CalculaArea() = 0;
 
 };
@@ -42,12 +44,11 @@ class Retangulo : public FiguraGeometrica{
 
     Retangulo(int x, int y, int comprimento, int largura) : FiguraGeometrica(x, y), comprimento(comprimento), largura(largura){}
 
-    void Retangulo::Desenha(){
-        FiguraGeometrica::Desenha();
+    void Desenha() override {
         cout << "RETANGULO" << endl;
     }
 
-    float Retangulo::CalculaArea(){
+    float CalculaArea() override{
         return this->comprimento * this->largura;
     }
 
@@ -61,12 +62,11 @@ class Triangulo : public FiguraGeometrica{
 
     Triangulo(int x, int y, int base, int altura) : FiguraGeometrica(x, y), base(base), altura(altura){}
 
-    void Triangulo::Desenha(){
-        FiguraGeometrica::Desenha();
+    void Desenha() override{
         cout << "Triangulo" << endl;
     }
 
-    float Triangulo::CalculaArea(){
+    float CalculaArea() override{
         return (this->base * this->altura) / 2;
     }
 
@@ -80,12 +80,11 @@ class Circulo : public FiguraGeometrica{
 
     Circulo(int x, int y, int raio) : FiguraGeometrica(x, y), raio(raio){}
 
-    void Circulo::Desenha(){
-        FiguraGeometrica::Desenha();
+    void Desenha() override{
         cout << "RETANGULO" << endl;
     }
 
-    float Circulo::CalculaArea(){
+    float CalculaArea() override{
         return PI*((this->raio * this->raio));
     }
 
@@ -95,13 +94,56 @@ class Circulo : public FiguraGeometrica{
 int main(){
 
 
+    vector <FiguraGeometrica*> figuras;
+
+    char opc;
+    int largura, comprimento, base, altura, raio;
+    int a, b;
+    float area = 0;
+
+    while(1){
     
+    cin >> opc;
+        
+        if(opc == 'E'){
+        
+        for(FiguraGeometrica* figura : figuras){
+            delete figura;
+        }
+            break;
+        }
+        else if(opc == 'R'){
+            cin >> a >> b >> comprimento >> largura;
+            figuras.push_back(new Retangulo(a, b, comprimento, largura));
+        }
+        else if(opc == 'C'){
+            cin >> a >> b >>  raio;
+            Circulo circulo(a, b, raio);
+            figuras.push_back(new Circulo(a, b, raio));
+        }
+        else if(opc == 'T'){
+            cin >> a, b, base, altura;
+            Triangulo triangulo(a, b, base, altura);
+            figuras.push_back(new Triangulo(a, b, base, altura));
+        }
+        else if(opc == 'D'){
+            for(FiguraGeometrica* figura : figuras){
+                Ponto ponto = figura->getPontoA();
+                cout << ponto.getX() << ponto.getY();
+                figura->Desenha();
+            }
+        }
+        else if(opc == 'A'){
+            for(FiguraGeometrica* figura : figuras){
+                area += figura->CalculaArea();
+            }
+            cout << area << endl;
+        }
+    }
 
-
-
-
-
-
+    for(FiguraGeometrica* figura : figuras){
+        delete figura;
+    }
 
 
     return 0;
